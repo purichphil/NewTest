198dbfc5f00a1f0071c0935f370b13ffd35c5a0c
Kandith Wongsuwan <ws.kandith@gmail.com>
2019-05-23 15:57:16 +0700

    try fix

--- testcpp.cpp
+++ testcpp.cpp
@@ -1,5 +1,16 @@
 #include<stdio.h>
 #include <iostream>
+#include <vector>
+
 int main(){
     std::cout << "Hello World!!" << std::endl;
+    std::cout << "Result :" << std::endl;        
+    std::vector<int> test(10);
+    
+    for (int i=0; i<10; i++){
+        test[i] = i * 2;
+        std::cout << test[i] << ',';
+    }
+
+    std::cout << std::endl;
 }
